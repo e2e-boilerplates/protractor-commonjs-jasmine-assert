191ca380f016c28c4dca91d593f800e785cda236
Girma Nigusse <xgirma@gmail.com>
2020-01-26 15:29:49 -0500

    update test

--- spec/search.spec.js
+++ spec/search.spec.js
@@ -1,8 +1,11 @@
 const assert = require("assert");
 
 describe("Sandbox", () => {
-  it("should be on the sandbox", async () => {
+  beforeAll(() => {
     browser.get("https://e2e-boilerplates.github.io/sandbox/");
+  });
+
+  it("should be on the sandbox", async () => {
     const title = await browser.getTitle();
     const header = element(by.css("h1"));
 
